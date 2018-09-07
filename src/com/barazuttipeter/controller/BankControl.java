4eb6c4279566cb3d678a44d4aad3a0cde56630cc
peterBarazutti <barazutti.peter@gmail.com>
2018-09-07 14:55:28 +0200

    Feature: making deposit/withdrawal, storing transfer in client account's history

--- src/com/barazuttipeter/controller/BankControl.java
+++ src/com/barazuttipeter/controller/BankControl.java
@@ -1,4 +1,28 @@
 package com.barazuttipeter.controller;
 
+import com.barazuttipeter.dao.DatabaseHandler;
+import com.barazuttipeter.models.ClientAccount;
+import com.barazuttipeter.models.Transfer;
+
+import java.math.BigDecimal;
+
 public class BankControl {
+
+    private DatabaseHandler db;
+
+    public BankControl(DatabaseHandler db) {
+        this.db = db;
+    }
+
+    public void depositWithdrawal(int id, int amount) {
+        BigDecimal balanceChange = new BigDecimal(amount);
+        ClientAccount account = db.getAccountById(id);
+        account.setBalance(
+                account
+                        .getBalance()
+                        .add(balanceChange)
+        );
+        account.getTransferHistory()
+                .add(new Transfer(amount, 0, id));
+    }
 }
