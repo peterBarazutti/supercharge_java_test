69aa9384e77bda956c1e9894225505598dfdcc77
peterBarazutti <barazutti.peter@gmail.com>
2018-09-07 14:45:37 +0200

    Refactor: added "Cash deposit" user representing manual money transfer,  fixed parameter type

--- src/com/barazuttipeter/dao/DatabaseHandler.java
+++ src/com/barazuttipeter/dao/DatabaseHandler.java
@@ -12,16 +12,17 @@ public class DatabaseHandler {
     private static List<ClientAccount> ClientAccountList;
 
     static {
-         ClientAccountList= new ArrayList(){
+        ClientAccountList = new ArrayList() {
             {
+                add(new ClientAccount("Cash Deposit"));
                 add(new ClientAccount("Client Katalin"));
                 add(new ClientAccount("Client Károly"));
-                add(new ClientAccount("Egyéb klines"));
+                add(new ClientAccount("Some Random Client"));
             }
         };
-         ClientAccountList.get(0).getTransferHistory().add(new Transfer((new BigDecimal(1000)), 0,1));
-         ClientAccountList.get(1).getTransferHistory().add(new Transfer((new BigDecimal(5000)), 0,2));
-         ClientAccountList.get(2).getTransferHistory().add(new Transfer((new BigDecimal(2300)), 0,3));
+        ClientAccountList.get(1).getTransferHistory().add(new Transfer(1000, 0, 1));
+        ClientAccountList.get(2).getTransferHistory().add(new Transfer(5000, 0, 2));
+        ClientAccountList.get(3).getTransferHistory().add(new Transfer(2300, 0, 3));
     }
-    
+
 }
