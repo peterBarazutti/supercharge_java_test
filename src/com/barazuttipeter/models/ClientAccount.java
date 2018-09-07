b8618af1d5d5c32472de6d2c359629d15ca2a6f5
peterBarazutti <barazutti.peter@gmail.com>
2018-09-07 14:10:29 +0200

    Model representing client's account created

--- src/com/barazuttipeter/models/ClientAccount.java
+++ src/com/barazuttipeter/models/ClientAccount.java
@@ -1,4 +1,34 @@
 package com.barazuttipeter.models;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class ClientAccount {
+
+    static int idCounter = 0;
+
+    private int id;
+    private String name;
+    private BigDecimal balance;
+    private List<Transfer> transferHistory;
+
+    public ClientAccount(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public List<Transfer> getTransferHistory() {
+        return transferHistory;
+    }
 }
