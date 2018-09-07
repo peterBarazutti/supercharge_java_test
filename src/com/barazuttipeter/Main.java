ba4b5f95bd70b159bb84eeb7d6d179a83973cb9d
peterBarazutti <barazutti.peter@gmail.com>
2018-09-07 16:19:16 +0200

    Test code

--- src/com/barazuttipeter/Main.java
+++ src/com/barazuttipeter/Main.java
@@ -1,4 +1,18 @@
 package com.barazuttipeter;
 
+import com.barazuttipeter.controller.BankControl;
+import com.barazuttipeter.dao.DatabaseHandler;
+import com.barazuttipeter.models.Transfer;
+
 public class Main {
-}
+
+    public static void main(String[] args) {
+        DatabaseHandler dbh = new DatabaseHandler();
+        BankControl bc = new BankControl(dbh);
+        bc.deposit(1,2000);
+        bc.deposit(1, 3000);
+        bc.printHistory(1);
+        bc.withdrawal(1,6000);
+        }
+    }
+
