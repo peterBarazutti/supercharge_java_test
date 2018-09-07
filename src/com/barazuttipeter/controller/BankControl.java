add67787b18d0dc4426f1b7333a34a5533a94050
peterBarazutti <barazutti.peter@gmail.com>
2018-09-07 15:08:58 +0200

    Refactor: separated deposit and withdrawal functions

--- src/com/barazuttipeter/controller/BankControl.java
+++ src/com/barazuttipeter/controller/BankControl.java
@@ -14,18 +14,13 @@ public class BankControl {
         this.db = db;
     }
 
-    public void depositWithdrawal(int id, int amount) {
-        BigDecimal balanceChange = new BigDecimal(amount);
-        ClientAccount account = db.getAccountById(id);
-        account.setBalance(
-                account
-                        .getBalance()
-                        .add(balanceChange)
-        );
-        account.getTransferHistory()
-                .add(new Transfer(amount, 0, id));
+    public void deposit(int id, int amount){
+        transfer(0,id,amount);
     }
 
+    public void withdrawal(int id, int amount){
+        transfer(id,0, amount);
+    }
 
     public void transfer(int senderId, int recipientId, int amount) {
         BigDecimal balanceChange = new BigDecimal(amount);
