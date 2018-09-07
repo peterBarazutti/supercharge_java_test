106f8bba28873efab169c9cb97bcd509cce90b09
peterBarazutti <barazutti.peter@gmail.com>
2018-09-07 15:26:00 +0200

    Handling wrong transaction in case of sender lacking the necessary money

--- src/com/barazuttipeter/controller/BankControl.java
+++ src/com/barazuttipeter/controller/BankControl.java
@@ -3,6 +3,7 @@ package com.barazuttipeter.controller;
 import com.barazuttipeter.dao.DatabaseHandler;
 import com.barazuttipeter.models.ClientAccount;
 import com.barazuttipeter.models.Transfer;
+import com.barazuttipeter.util.NotEnoughMoneyExc;
 
 import java.math.BigDecimal;
 
@@ -14,34 +15,41 @@ public class BankControl {
         this.db = db;
     }
 
-    public void deposit(int id, int amount){
-        transfer(0,id,amount);
+    public void deposit(int id, int amount) {
+        transfer(0, id, amount);
     }
 
-    public void withdrawal(int id, int amount){
-        transfer(id,0, amount);
+    public void withdrawal(int id, int amount) {
+        transfer(id, 0, amount);
     }
 
     public void transfer(int senderId, int recipientId, int amount) {
-        BigDecimal balanceChange = new BigDecimal(amount);
-        Transfer transfer = new Transfer(amount, senderId, recipientId);
-        ClientAccount senderAccount = db.getAccountById(senderId);
-        senderAccount.setBalance(
-                senderAccount
-                        .getBalance()
-                        .subtract(balanceChange)
-        );
-        senderAccount.getTransferHistory()
-                .add(transfer);
+        try {
+            BigDecimal balanceChange = new BigDecimal(amount);
+            Transfer transfer = new Transfer(amount, senderId, recipientId);
+            ClientAccount senderAccount = db.getAccountById(senderId);
+            if (balanceChange.compareTo(senderAccount.getBalance()) == 1 && senderId != 0) {
+                throw new NotEnoughMoneyExc("Sender is poor");
+            }
+            senderAccount.setBalance(
+                    senderAccount
+                            .getBalance()
+                            .subtract(balanceChange)
+            );
+            senderAccount.getTransferHistory()
+                    .add(transfer);
 
-        ClientAccount recipientAccount = db.getAccountById(recipientId);
-        recipientAccount.setBalance(
-                recipientAccount
-                        .getBalance()
-                        .add(balanceChange)
-        );
-        recipientAccount.getTransferHistory()
-                .add(transfer);
+            ClientAccount recipientAccount = db.getAccountById(recipientId);
+            recipientAccount.setBalance(
+                    recipientAccount
+                            .getBalance()
+                            .add(balanceChange)
+            );
+            recipientAccount.getTransferHistory()
+                    .add(transfer);
+        } catch (NotEnoughMoneyExc e) {
+            e.printStackTrace();
+        }
     }
 
 }
